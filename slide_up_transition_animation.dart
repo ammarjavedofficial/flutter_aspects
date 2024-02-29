7dcd4e09700c5252aefab7da00c2d7fd91eb36ae
Ammar Javed <ammarjavedofficial@gmail.com>
2024-02-29 16:03:57 +0500

    Update slide_up_transition_animation.dart

--- slide_up_transition_animation.dart
+++ slide_up_transition_animation.dart
@@ -1,333 +1,180 @@
 import 'package:flutter/material.dart';
 
-List<String> imagesList = [
+List<String> imageList = [
+  'imagePath',
   'imagePath',
   'imagePath',
 ];
 
-class SlideUpTransitionAnimation extends StatefulWidget {
-  const SlideUpTransitionAnimation({super.key});
+class FadeAndMoveUpWardAnimation extends StatefulWidget {
+  const FadeAndMoveUpWardAnimation({super.key});
 
   @override
-  State<SlideUpTransitionAnimation> createState() =>
-      _SlideUpTransitionAnimationState();
+  State<FadeAndMoveUpWardAnimation> createState() => _FadeAndMoveUpWardAnimationState();
 }
 
-class _SlideUpTransitionAnimationState
-    extends State<SlideUpTransitionAnimation> {
-  int currentPageIndex = 0;
+class _FadeAndMoveUpWardAnimationState extends State<FadeAndMoveUpWardAnimation> {
+  int _currentPage = 0;
+
+  PageController pageController = PageController(
+    viewportFraction: 0.8,
+  );
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Image ${currentPageIndex + 1} title here'
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: PageView.builder(
-          itemCount: imagesList.length,
-          onPageChanged: (index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          itemBuilder: (context, index) {
-            return PageViewItem(
-              imagePath: imagesList[index],
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class PageViewItem extends StatefulWidget {
-  const PageViewItem({super.key, required this.imagePath});
-
-  final String imagePath;
-
-  @override
-  State<PageViewItem> createState() => _PageViewItemState();
-}
-
-class _PageViewItemState extends State<PageViewItem> {
-  bool isExpanded = false;
-  double _opacity = 0.0;
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        AnimatedOpacity(
-          duration: const Duration(milliseconds: 500),
-          opacity: _opacity,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.asset(
-              widget.imagePath,
-            ),
-          ),
-        ),
-        AnimatedPositioned(
-          duration: const Duration(milliseconds: 500),
-          bottom: isExpanded ? 80 : 100,
-          width: isExpanded ? width * 0.78 : width * 0.7,
-          height: isExpanded ? height * 0.5 : height * 0.5,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Hero(
-                  tag: 'titleHeroTag',
-                  child: Material(
-                    type: MaterialType.transparency,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Title Here',
-                          style: TextStyle(
-                            height: 0,
-                            fontSize: 24,
-                          ),
-                        ),
-                        Text(
-                          'Subtitle Here',
-                          style: TextStyle(
-                            fontSize: 12,
-                            height: 0,
-                            color: Colors.black45,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: height * 0.045,
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 4,
-                    separatorBuilder: (context, index) => SizedBox(
-                      width: width * 0.005,
-                    ),
-                    itemBuilder: (context, index) {
-                      return Hero(
-                        tag: 'CircleTag$index',
-                        child: const CircleAvatar(
-                          backgroundImage: AssetImage('imagePath'),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        AnimatedPositioned(
-          duration: const Duration(milliseconds: 500),
-          bottom: isExpanded ? 150 : 100,
-          child: GestureDetector(
-            onPanUpdate: (details) {
-              //top
-              if (details.delta.dy < 0) {
-                setState(() {
-                  _opacity = 1.0;
-                  isExpanded = true;
-                });
-              }
-              //bottom
-              else if (details.delta.dy > 0) {
-                setState(() {
-                  _opacity = 0.0;
-                  isExpanded = false;
-                });
-              }
-            },
-            onTap: () {
-              if (!isExpanded) {
-                setState(() {
-                  isExpanded = true;
-                });
-                return;
-              }
-              Navigator.of(context).push(PageRouteBuilder(
-                transitionDuration: const Duration(seconds: 1),
-                reverseTransitionDuration: const Duration(seconds: 1),
-                pageBuilder: (context, animation, secondaryAnimation) {
-                  final curvedAnimation = CurvedAnimation(
-                    parent: animation,
-                    curve: const Interval(0, 0.5),
-                  );
-                  return FadeTransition(
-                    opacity: curvedAnimation,
-                    child: DetailView(
-                      animation: animation,
-                      imagePath: widget.imagePath,
-                    ),
-                  );
-                },
-              ));
-            },
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.white24,
-                    blurRadius: 15,
-                    spreadRadius: 0.5,
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Hero(
-                  tag: 'image',
-                  child: Image.asset(
-                     widget.imagePath,
-                     height: height * 0.65,
-                    width: width * 0.7,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class DetailView extends StatelessWidget {
-  const DetailView({super.key, required this.animation, required this.imagePath, });
-
-  final Animation<double> animation;
-  final String imagePath;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          Hero(
-            tag: 'image',
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(16),
-                bottomRight: Radius.circular(16),
-              ),
-              child: Image.asset(
-               imagePath,
-                fit: BoxFit.cover,
-                height: 540,
-                width: double.infinity,
-              ),
-            ),
-          ),
-          Hero(
-            tag: 'titleHeroTag',
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: const Material(
-                type: MaterialType.transparency,
-                child: SizedBox(
-                  width: 200,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Title Here',
-                        style: TextStyle(
-                          height: 0,
-                          fontSize: 24,
-                        ),
-                      ),
-                      Text(
-                        'Subtitle Here',
-                        style: TextStyle(
-                          fontSize: 12,
-                          height: 0,
-                          color: Colors.black45,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: AnimatedBuilder(
-              animation: animation,
-              builder: (context, child) {
-                return FadeTransition(
-                  opacity: CurvedAnimation(
-                    parent: animation,
-                    curve: const Interval(
-                      0.2,
-                      1,
-                      curve: Curves.easeInExpo,
-                    ),
-                  ),
-                  child: child,
-                );
-              },
-              child: ListView.separated(
-                itemCount: 4,
-                separatorBuilder: (context, index) => const SizedBox(
-                  height: 10,
-                ),
-                padding: EdgeInsets.zero,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Hero(
-                      tag: 'CircleTag$index',
-                      child: const CircleAvatar(
-                        backgroundImage: AssetImage('imagePath'),
-                      ),
-                    ),
-                    minLeadingWidth: 50,
-                    title: const Text(
-                      'Username',
-                    ),
-                    subtitle: const Text(
-                      'Comment',
-                      style: TextStyle(
-                        color: Colors.black54,
-                      ),
-                    ),
-                    trailing: const Text(
-                      '12: 00 PM',
-                      style: TextStyle(
-                        color: Colors.black26,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+          BackImageWidget(currentPage: _currentPage),
+          BottomGradientWidget(),
+          PageView.builder(
+            controller: pageController,
+            itemCount: imageList.length,
+            onPageChanged: (value) {
+              _currentPage = value;
+              setState(() {});
+            },
+            itemBuilder: (context, index) {
+              return PageViewItem(currentPage: _currentPage, index: index);
+            },
           ),
         ],
       ),
+    );
+  }
+}
+
+class PageViewItem extends StatelessWidget {
+  const PageViewItem({
+    super.key,
+    required int currentPage,
+    required int index,
+  }) : _currentPage = currentPage, _index = index;
+
+  final int _currentPage;
+  final int _index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: ColoredBox(
+            color: Colors.white,
+            child: AnimatedContainer(
+              padding: EdgeInsets.all(16),
+              duration: Duration(milliseconds: 500),
+              height: _currentPage == _index ? 590 : 390,
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                runSpacing: 15,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(25),
+                    child: Image.asset(
+                      imageList[_index],
+                      fit: BoxFit.fill,
+                      width: double.infinity,
+                      height: _currentPage == _index ? 500 : 350,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Title',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                        ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Colors.green.shade600),
+                      minimumSize: MaterialStatePropertyAll(Size(50, 40))
+                    ),
+                    child: Text(
+                      'Details',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                    ],
+                  ),
+                
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomGradientWidget extends StatelessWidget {
+  const BottomGradientWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.white.withOpacity(0.0001),
+            Colors.white,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+    );
+  }
+}
+
+class BackImageWidget extends StatelessWidget {
+  const BackImageWidget({
+    super.key,
+    required int currentPage,
+  }) : _currentPage = currentPage;
+
+  final int _currentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 500),
+      child: Image.asset(
+        imageList.elementAt(_currentPage),
+        key: Key(imageList.elementAt(_currentPage)),
+        fit: BoxFit.cover,
+      ),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return FadeTransition(
+          opacity: animation,
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: child,
+          ),
+        );
+      },
     );
   }
 }
